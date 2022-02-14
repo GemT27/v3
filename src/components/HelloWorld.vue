fcd2f7248916c1d41cfbfea310e8986feca3de2d
TT527 <t0527t@126.com>
2022-02-14 09:19:38 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  props: {
+    msg: String
+  },
+
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
